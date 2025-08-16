af3ca75c263cb33550a60ca678d9fcf73d7d4438
Tushar Pal <tushrpal@gmail.com>
2025-08-16 17:34:39 +0530

    feat: added animated icon for widget button

--- src/Component/Widget/WidgetButton.tsx
+++ src/Component/Widget/WidgetButton.tsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import { MessageSquareHeart } from "lucide-react";
 import "./Widget.css";
+import msgGif from "../../assets/message-square-heart_1.gif";
 
 type Props = {
   onWidgetClick: () => void;
 };
 
 export default function WidgetButton({ onWidgetClick }: Props) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <button
       onClick={onWidgetClick}
       className="widget-button"
       data-testid="widget-button"
       aria-label="Open feedback form"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
-      <MessageSquareHeart />
+      {hovered ? (
+        <img src={msgGif} alt="heart message gif" className="icon-gif" />
+      ) : (
+        <MessageSquareHeart />
+      )}
     </button>
   );
 }
